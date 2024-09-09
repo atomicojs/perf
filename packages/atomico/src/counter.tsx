b5c9b5dccba84140d2ee387efdb0a3bab83e71f1
MatiasWorker <matias.trujillo@blue.cl>
2024-09-08 23:24:31 -0300

    Update dependencies

--- packages/atomico/src/counter.tsx
+++ packages/atomico/src/counter.tsx
@@ -1,39 +1,39 @@
 import { c, useProp, css } from "atomico";
 
-function counter() {
-    let [count, setCount] = useProp("count");
-    return (
-        <host shadowDom>
-            <button onclick={() => setCount(count - 1)}>-</button>
-            <span>{count}</span>
-            <button onclick={() => setCount(count + 1)}>+</button>
-        </host>
-    );
-}
+export const Counter = c(
+    () => {
+        let [count, setCount] = useProp<number>("count");
+        return (
+            <host shadowDom>
+                <button onclick={() => setCount(count - 1)}>-</button>
+                <span>{count}</span>
+                <button onclick={() => setCount(count + 1)}>+</button>
+            </host>
+        );
+    },
+    {
+        props: { count: { type: Number, value: 0 } },
+        styles: css`
+            * {
+                font-size: 200%;
+            }
 
-counter.props = { count: { type: Number, value: 0 } };
+            span {
+                width: 4rem;
+                display: inline-block;
+                text-align: center;
+            }
 
-counter.styles = css`
-    * {
-        font-size: 200%;
+            button {
+                width: 4rem;
+                height: 4rem;
+                border: none;
+                border-radius: 10px;
+                background-color: seagreen;
+                color: white;
+            }
+        `,
     }
-
-    span {
-        width: 4rem;
-        display: inline-block;
-        text-align: center;
-    }
-
-    button {
-        width: 4rem;
-        height: 4rem;
-        border: none;
-        border-radius: 10px;
-        background-color: seagreen;
-        color: white;
-    }
-`;
-
-export const Counter = c(counter);
+);
 
 customElements.define("my-counter", Counter);
